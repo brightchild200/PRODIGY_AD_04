efefbec2be920d734e53aab9fcdea49b16a17d2d
brightchild200 <brightchild.riya@gmail.com>
2025-06-17 16:00:36 +0530

    designing ui

---
+++ app/src/main/java/com/example/tictactoe/GameActivity.kt
@@ -0,0 +1,25 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class GameActivity : AppCompatActivity(){
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.playOffline.setOnClickListener {
+            createOfflineGame()
+        }
+    }
+
+    fun createOfflineGame() {
+        startGame()
+    }
+
+    fun startGame() {
+        startActivity(Intent(this, GameActivity::class.java))
+    }
+}
